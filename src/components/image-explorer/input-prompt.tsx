02b0f1e5a2098900b101cf80318d12432e17c395
zys5945 <shawnl9025@gmail.com>
2025-06-10 15:00:47 -0700

    disallow selection on input-prompt

--- src/components/image-explorer/input-prompt.tsx
+++ src/components/image-explorer/input-prompt.tsx
@@ -2,7 +2,7 @@ import { Upload } from "lucide-react";
 
 export function InputPrompt() {
   return (
-    <div className="relative w-full h-full transition-all ease-in-out duration-300 hover:scale-120">
+    <div className="relative w-full h-full transition-all ease-in-out duration-300 hover:scale-120 select-none">
       <div className="w-full h-full flex flex-col items-center justify-center h-full space-y-2 ">
         <Upload className="h-12 w-12" />
         <p className="text-lg font-medium dark:text-gray-300">
