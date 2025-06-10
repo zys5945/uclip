3982abc50b3966b80307ebe66d6ae1e4fa9d2f11
zys5945 <shawnl9025@gmail.com>
2025-06-09 17:02:59 -0700

    move toolbar into separate component

--- src/components/image-editor/index.tsx
+++ src/components/image-editor/index.tsx
@@ -1,32 +1,16 @@
-import { KeyboardEvent, useEffect, useMemo, useRef, useState } from "react";
+import { KeyboardEvent, useEffect, useMemo, useRef } from "react";
 
-import {
-  CropIcon,
-  MoveIcon,
-  PencilIcon,
-  SearchIcon,
-  UndoIcon,
-  RedoIcon,
-} from "lucide-react";
-
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { CropTool, CropToolSubToolbar } from "./crop";
-import { EditContext, EditData, EditTool } from "./edit-context";
-import { PanTool } from "./pan";
-import { ZoomTool, ZoomToolSubToolbar } from "./zoom";
-import { PenTool } from "./pen";
+import { EditContext, EditData } from "./edit-context";
+import { Toolbar, ToolbarHandle } from "./toolbar";
 
 export interface ImageEditorProps {
   image?: string;
 }
 
 export function ImageEditor({ image }: ImageEditorProps) {
-  const containerRef = useRef<HTMLDivElement>(null);
+  const toolbarRef = useRef<ToolbarHandle>(null);
   const canvasContainerRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [currentToolName, setCurrentToolName] = useState<string | undefined>(
-    void 0
-  );
 
   let editContext = useMemo(() => new EditContext(), []);
   let editData = useMemo(() => new EditData(), [image]);
@@ -38,7 +22,9 @@ export function ImageEditor({ image }: ImageEditorProps) {
     if (!editContext.initialized) {
       editContext.init(canvasRef.current);
     }
-    switchTool("pan");
+    if (toolbarRef.current) {
+      toolbarRef.current.setTool("pan");
+    }
 
     // TODO
     const imageData = editContext.setImage(imageElement);
@@ -81,148 +67,41 @@ export function ImageEditor({ image }: ImageEditorProps) {
     return () => observer.disconnect();
   }, [canvasRef, canvasContainerRef]);
 
-  const switchTool = (
-    toolName: "pan" | "zoom" | "crop" | "pen" | "undo" | "redo"
-  ) => {
-    if (!editContext.initialized) return;
+  // const handleKeyDown = (e: KeyboardEvent) => {
+  //   // deactivate current tool on escape
+  //   if (e.key === "Escape") {
+  //     switchTool("pan");
+  //     return;
+  //   }
 
-    if (toolName === "undo") {
-      editContext.data.undo();
-    } else if (toolName === "redo") {
-      editContext.data.redo();
-    } else {
-      // deactivate the current active tool. however pan tool cannot be manually deactivated
-      if (toolName === currentToolName && toolName === "pan") return;
-      editContext.deactivateTool();
-
-      const nextToolName = toolName === currentToolName ? "pan" : toolName;
-      let tool!: EditTool;
-      switch (nextToolName) {
-        case "pan":
-          tool = new PanTool();
-          break;
-        case "zoom":
-          tool = new ZoomTool();
-          break;
-        case "crop":
-          tool = new CropTool();
-          break;
-        case "pen":
-          tool = new PenTool();
-          break;
-      }
-
-      editContext.setTool(tool);
-      setCurrentToolName(nextToolName);
-    }
-
-    /**
-     * prevents losing focus due to sub toolbar getting removed
-     * losing focus will lead to keyboard shortcuts not working
-     */
-    if (containerRef.current) {
-      containerRef.current.focus();
-    }
-  };
-
-  const handleKeyDown = (e: KeyboardEvent) => {
-    // deactivate current tool on escape
-    if (e.key === "Escape") {
-      switchTool("pan");
-      return;
-    }
-
-    if (!e.ctrlKey) return;
-    switch (e.key) {
-      // ctrl + num to switch tool
-      case "1":
-        switchTool("pan");
-        break;
-      case "2":
-        switchTool("zoom");
-        break;
-      case "3":
-        switchTool("crop");
-        break;
-      case "4":
-        switchTool("pen");
-        break;
-      case "z":
-        if (e.shiftKey) {
-          switchTool("redo");
-        } else {
-          switchTool("undo");
-        }
-        break;
-    }
-  };
-
-  const renderSubToolbar = () => {
-    let subToolbar;
-
-    switch (currentToolName ?? 0) {
-      case "zoom":
-        subToolbar = <ZoomToolSubToolbar ctx={editContext} />;
-        break;
-      case "crop":
-        subToolbar = (
-          <CropToolSubToolbar
-            ctx={editContext}
-            onExit={() => switchTool("pan")}
-          />
-        );
-        break;
-    }
-
-    if (subToolbar) {
-      return (
-        <div className="flex space-x-2 p-1 bg-stone-700 rounded-md">
-          {subToolbar}
-        </div>
-      );
-    }
-  };
+  //   if (!e.ctrlKey) return;
+  //   switch (e.key) {
+  //     // ctrl + num to switch tool
+  //     case "1":
+  //       switchTool("pan");
+  //       break;
+  //     case "2":
+  //       switchTool("zoom");
+  //       break;
+  //     case "3":
+  //       switchTool("crop");
+  //       break;
+  //     case "4":
+  //       switchTool("pen");
+  //       break;
+  //     case "z":
+  //       if (e.shiftKey) {
+  //         switchTool("redo");
+  //       } else {
+  //         switchTool("undo");
+  //       }
+  //       break;
+  //   }
+  // };
 
   return image ? (
-    <div
-      className="w-full h-full flex flex-col space-y-0"
-      tabIndex={1}
-      onKeyDown={handleKeyDown}
-      ref={containerRef}
-    >
-      <div className="flex p-2 space-x-2">
-        {/* main toolbar */}
-        <div className="flex space-x-2 p-1 bg-stone-700 rounded-md">
-          <ToggleGroup
-            type="single"
-            className="gap-1"
-            value={currentToolName}
-            onValueChange={(value) => switchTool((value as any) || "pan")}
-          >
-            <ToggleGroupItem value="pan">
-              <MoveIcon />
-            </ToggleGroupItem>
-            <ToggleGroupItem value="zoom">
-              <SearchIcon />
-            </ToggleGroupItem>
-            <ToggleGroupItem value="crop">
-              <CropIcon />
-            </ToggleGroupItem>
-            <ToggleGroupItem value="pen">
-              <PencilIcon />
-            </ToggleGroupItem>
-            <ToggleGroupItem value="undo">
-              <UndoIcon />
-            </ToggleGroupItem>
-            <ToggleGroupItem value="redo">
-              <RedoIcon />
-            </ToggleGroupItem>
-          </ToggleGroup>
-        </div>
-
-        {/* sub toolbar */}
-        {renderSubToolbar()}
-      </div>
+    <div className="w-full h-full flex flex-col space-y-0">
+      <Toolbar ctx={editContext} ref={toolbarRef} />
 
       <div className="flex-1 w-full" ref={canvasContainerRef}>
         <canvas ref={canvasRef} width={800} height={600} />
---
+++ src/components/image-editor/toolbar.tsx
@@ -0,0 +1,145 @@
+import {
+  CropIcon,
+  MoveIcon,
+  PencilIcon,
+  RedoIcon,
+  SearchIcon,
+  UndoIcon,
+} from "lucide-react";
+import { useState, useImperativeHandle, useEffect } from "react";
+
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { CropTool, CropToolSubToolbar } from "./crop";
+import { EditContext, EditTool } from "./edit-context";
+import { PanTool } from "./pan";
+import { PenTool } from "./pen";
+import { ZoomTool, ZoomToolSubToolbar } from "./zoom";
+
+type ToolName = "pan" | "zoom" | "crop" | "pen" | "undo" | "redo";
+
+export interface ToolbarHandle {
+  setTool: (toolName: ToolName) => void;
+}
+
+export function Toolbar({
+  ctx,
+  ref,
+}: {
+  ctx: EditContext;
+  ref: React.Ref<ToolbarHandle>;
+}) {
+  const [currentToolName, setCurrentToolName] = useState<string | undefined>(
+    void 0
+  );
+
+  const setTool = (toolName: ToolName) => {
+    if (!ctx.initialized) return;
+
+    if (toolName === "undo") {
+      ctx.data.undo();
+      return;
+    }
+
+    if (toolName === "redo") {
+      ctx.data.redo();
+      return;
+    }
+
+    // deactivate the current active tool. however pan tool cannot be manually deactivated
+    if (toolName === currentToolName && toolName === "pan") return;
+    ctx.deactivateTool();
+
+    const nextToolName = toolName === currentToolName ? "pan" : toolName;
+    let tool!: EditTool;
+    switch (nextToolName) {
+      case "pan":
+        tool = new PanTool();
+        break;
+      case "zoom":
+        tool = new ZoomTool();
+        break;
+      case "crop":
+        tool = new CropTool();
+        break;
+      case "pen":
+        tool = new PenTool();
+        break;
+    }
+
+    ctx.setTool(tool);
+    setCurrentToolName(nextToolName);
+  };
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        setTool,
+      };
+    },
+    []
+  );
+
+  useEffect(() => {
+    setTool("pan");
+  }, []);
+
+  const renderSubToolbar = () => {
+    let subToolbar;
+
+    switch (currentToolName ?? 0) {
+      case "zoom":
+        subToolbar = <ZoomToolSubToolbar ctx={ctx} />;
+        break;
+      case "crop":
+        subToolbar = (
+          <CropToolSubToolbar ctx={ctx} onExit={() => setTool("pan")} />
+        );
+        break;
+    }
+
+    if (subToolbar) {
+      return (
+        <div className="flex space-x-2 p-1 bg-stone-700 rounded-md">
+          {subToolbar}
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="flex p-2 space-x-2">
+      {/* main toolbar */}
+      <div className="flex space-x-2 p-1 bg-stone-700 rounded-md">
+        <ToggleGroup
+          type="single"
+          className="gap-1"
+          value={currentToolName}
+          onValueChange={(value) => setTool((value as any) || "pan")}
+        >
+          <ToggleGroupItem value="pan">
+            <MoveIcon />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="zoom">
+            <SearchIcon />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="crop">
+            <CropIcon />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="pen">
+            <PencilIcon />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="undo">
+            <UndoIcon />
+          </ToggleGroupItem>
+          <ToggleGroupItem value="redo">
+            <RedoIcon />
+          </ToggleGroupItem>
+        </ToggleGroup>
+      </div>
+
+      {/* sub toolbar */}
+      {renderSubToolbar()}
+    </div>
+  );
+}
